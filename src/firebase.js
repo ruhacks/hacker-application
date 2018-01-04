f70f580ae22c5ef8a5f5bbeb41a1712a568414c4
ktsuench <will289@hotmail.com>
2018-01-04 02:24:32 -0500

    added in firebase.js back to project, sensitive config details not included

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+import * as firebase from 'firebase';
+import * as config from './firebase.json';
+
+export const firebaseApp = firebase.initializeApp(config);
+
+   // export const provider = new firebase.auth.GoogleAuthProvider();
+   // export const auth = firebase.auth();
+   // export const database = firebase.database();
+
