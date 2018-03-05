9cb554fd4ff5d309a037880800e14dda010aeccc
ktsuench <will289@hotmail.com>
2018-03-04 23:27:07 -0500

    updated title margin for messages in application

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -599,7 +599,7 @@ class Application extends Component {
               ReactDom.render(
                 <Bulma.Message success id='form-success-msg'>
                   <Bulma.Message.Header>
-                    <p>Info</p>
+                    <p style={{margin: 0}}>Info</p>
                     <Bulma.Delete
                       onClick={() => {
                         document
@@ -654,7 +654,7 @@ class Application extends Component {
               ReactDom.render(
                 <Bulma.Message danger id='form-error-msg'>
                   <Bulma.Message.Header>
-                    <p>Error</p>
+                    <p style={{margin: 0}}>Error</p>
                     <Bulma.Delete
                       onClick={() => {
                         document
@@ -685,7 +685,7 @@ class Application extends Component {
           ReactDom.render(
             <Bulma.Message danger id='form-validation-error-msg'>
               <Bulma.Message.Header>
-                <p>Error</p>
+                <p style={{margin: 0}}>Error</p>
                 <Bulma.Delete
                   onClick={() => {
                     document
